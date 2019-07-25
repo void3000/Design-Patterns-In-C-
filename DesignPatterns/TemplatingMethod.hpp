727afc8b4fdc80f6022037c073b7f820bff2dc46
void3000 <keorapetse.finger@yahoo.com>
2019-07-25 08:02:29 +0800

    Added UML diagrams

--- DesignPatterns/TemplatingMethod.hpp
+++ DesignPatterns/TemplatingMethod.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 class TravelTemplate
 {
 public:
@@ -25,3 +27,16 @@ public:
 	void buy_ticket() override;
 	void have_fun() override;
 };
+
+void ConcreteTravel::schedule()
+{
+	std::cout << "Scheduling...\n";
+}
+void ConcreteTravel::buy_ticket()
+{
+	std::cout << "Buy ticket...\n";
+}
+void ConcreteTravel::have_fun()
+{
+	std::cout << "Have fun...\n";
+}
